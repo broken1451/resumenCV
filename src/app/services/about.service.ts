d8ca35597bafb553c656d5ffa236786275301a5c
abravo <abravo@acid.cl>
2024-09-12 19:55:28 -0300

    cv pena

--- src/app/services/about.service.ts
+++ src/app/services/about.service.ts
@@ -6,142 +6,105 @@ export class AboutService {
 
 
     private _about = signal<string>(`
-        Computer Engineer with experience in customer service, responsible, capable of creating ideas
-        and initiatives for problem solving, teamwork and delegation of activities. Punctual, organized
-        and responsible. I am a very cheerful, optimistic and enthusiastic person. With a great desire to
-        grow professionally in the area of information and communication technologies. With experience in the development of web and mobile systems, general knowledge of Administration of
-        Windows and Linux platforms, Basic Shell Script, HTML, CSS, JavaScript, Bootstrap, Jquery,
-        Angular, NodeJs, NestJS , MongoDB, GIT, TypeScript, development of WEB applications using the
-        Angular framework (JavaScript), CMS(Wordpress, Woocommerce & E-commerce), Vmware,
-        Jmeter, Apache, AWS, MySql, unity testing / integral testing.`
+        Técnico superior en Diseño Gráfico, estoy familiarizado con el uso de diversos programas bajo Windows y MacOS, incluyendo Photoshop, Illustrator, In Design, After Effects y Adobe Premier. Ademas de tener habilidades para toma y retoque de fotografías, asi como rodaje de video para uso publicitario en redes sociales y otros medios, gigantografía y POP.`
     );
 
 
     private skills = signal<{ id: number, value: string }[]>(
         [
-            { id: 1, value: 'Angular' },
-            { id: 2, value: 'MongoDB' },
-            { id: 3, value: 'JavaScript' },
-            { id: 4, value: 'TypeScript' },
-            { id: 5, value: 'NodeJS' },
-            { id: 6, value: 'Express' },
-            { id: 7, value: 'Bootstrap' },
-            { id: 8, value: 'CSS' },
-            { id: 9, value: 'HTML' },
-            { id: 10, value: 'Git' },
-            { id: 21, value: 'NestJS' },
-            { id: 22, value: 'Golang' },
-            { id: 23, value: 'Docker' },
-            { id: 16, value: 'Ubuntu GNU/Linux' },
-            { id: 17, value: 'Debian GNU/Linux' },
-            { id: 18, value: 'JSON' },
-            { id: 19, value: 'Scrum' },
-            { id: 20, value: 'AWS' },
-            { id: 11, value: 'GitHub & GitLab' },
-            { id: 12, value: 'Xcode' },
-            { id: 13, value: 'Shell Scripting Basic' },
-            { id: 14, value: 'MySql' },
-            { id: 15, value: 'AJAX' }
+            { id: 1, value: 'PHOTOSHOP' },
+            { id: 2, value: 'ILLUSTRATOR' },
+            { id: 3, value: 'AFTER EFFECTS' },
+            { id: 4, value: 'ADOBE PREMIERE' },
+            { id: 5, value: 'IN DESIGN' },
+            { id: 6, value: 'FOTOGRAFÍA' },
+            { id: 7, value: 'RODAJE DE VIDEOS' },
+            { id: 8, value: 'CONTENIDO PARA RRSS' },
         ]
     );
 
     private _user = signal<BasicInfo>({
-        address: 'Camino San Miguel 350, Talca',
+        address: 'Marin 145, Santiago',
         country: 'Chile',
-        email: 'adrianbravo145@gmail.com',
-        phone: '+56936742459',
-        degree: 'Computer Engineer',
-        job: 'Full Stack Developer',
-        name: 'Adrian Bravo'
+        email: 'dannybdp92@gmail.com',
+        phone: '+56932441382',
+        degree: 'Diseño Gráfico',
+        job: 'Diseñador Gráfico',
+        name: 'Daniel Peña'
     });
 
     private _education = signal<Education[]>([
         {
-            title: 'Computer Engineer',
-            institution: 'IUT (Venezuela)',
-            name: 'Completed a Computer Engineering program at Instituto Universitario Doctor Dederico rivero palacios.',
-            date: '2015 - 2016',
+            title: 'Técnico Superior Universitario en la mención Diseño Gráfico.',
+            institution: 'IUTIRLA',
+            name: 'Instituto Universitario de Tecnología Industrial "Rodolfo Loero Arismendi.',
+            date: '2011 - 2015',
 
         },
         {
-            title: 'Informatics Certificate of Higher Education',
-            institution: 'IUT (Venezuela)',
-            name: 'Obtained an Informatics Certificate of Higher Education from Instituto Universitario Doctor Dederico rivero palacios.',
-            date: '2008 - 2014',
-        },
-        {
-            title: 'UDEMY',
-            institution: 'UDEMY',
-            date: '2018',
-            name: 'Completed a course on UDEMY.'
-        },
-        {
-            title: 'Angular & Angular 8: Basic to Advanced Course',
-            institution: 'UDEMY',
-            date: '2019',
-            name: 'Completed a course on Angular & Angular 8: Basic to Advanced Course on UDEMY.'
+            title: 'E.T.C. “DR. JESÚS MUÑÓZ TEBAR”',
+            institution: 'E.T.C. “DR. JESÚS MUÑÓZ TEBAR”',
+            name: 'Técnico Médio en la mención Informática.',
+            date: '2008 - 2010',
         }
     ]);
 
     private _experience = signal<Experience[]>([
         {
-            title: 'Full Stack Developer',
-            company: 'Acid Labs',
-            date: '2020 - Current ',
+            title: 'Disenador Freelance',
+            company: 'ACTIMEDIA DIGITAL (MIAMI)',
+            date: 'FEB 2021 - ACTUAL',
             description: [
                 {
                     id: 1,
-                    desc: `Web Application Development under the JavaScript, HTML5,CSS3 programming language and JavaScript-based frameworks.`
+                    desc: `Trabajo en modalidad freelance remoto con trabajo asignado por medio de calendario digital. Me encargo del contenido de RRSS de diferentes clientes asignados por mi jefe directo, incluyendo parte del contenido de productos P.A.N. y Acer.`
                 },
                 {
                     id: 2,
-                    desc: `Using Angular as frontend framework, using technologies such as NodeJs (Express) on the backend.`
-                },
-                {
-                    id: 3,
-                    desc: `Microservices and cronjobs development in golang (Go).`
-                },
-                {
-                    id: 8,
-                    desc: `Development API RESTFULL on Netsjs and golang.`
+                    desc: `Creación de videos animados para redes sociales, retoque fotográfico y foto-montajes, edición de videos con contenido original, reedición de spots publicitarios, entre otras responsabilidades.`
                 }
             ]
         },
         {
-            title: 'FrontEnd Web / Mobile Developer',
-            company: 'Inovabiz SPA',
-            date: '2020 - 2020',
-          
+            title: 'Diseñador Freelance',
+            company: 'COMERCIAL TOROGOZ',
+            date: 'FEB 2018 - SEPT 2019',
+
             description: [
                 {
-                    id: 4,
-                    desc: `Development of mobile applications under the programming
-                        language JavaScript, HTML5, CSS3 and frameworks based on
-                        JavaScript such as Angular with the integration of Ionic.`
-                },
-                {
-                    id: 5,
-                    desc: `On the
-                        backend branch using technologies such as NodeJs (Express)
-                        and NestJs`
-                },
+                    id: 3,
+                    desc: `Con Torogoz logré desarrollarme como diseñador freelance creando material visual para impulsar las ventas de la empresa por medio de publicaciones y vídeos para redes sociales.`
+                }
             ]
         },
         {
-            title: 'FrontEnd Web Developer',
-            company: 'Emergya Consultants',
-            date: '2019 - 2020',
+            title: 'Diseñador Gráfico Senior',
+            company: 'REPRESENTACIONES MULCOMANIA',
+            date: 'AGO 2016 - ENE 2019',
             description: [
                 {
-                    id: 6,
-                    desc:  `Development of Web Applications under the programming language JavaScript, HTML5, CSS3 and JavaScript-based frameworks such as Angular + 4.`
+                    id: 4,
+                    desc: `Diseño y actualización de catálogos de productos para ventas online. Así como también el diseño de distintos materiales para publicidad web de las marcas en venta, manejo de redes sociales, plataformas de ventas y optimi- zación de fotografías para ventas online en Venezuela, Chile, entre otros países latinoamericanos.`
+                },
+               
+            ]
+        },
+        {
+            title: 'Diseñador Gráfico Junior',
+            company: 'GRUPO TSWATCH TECHNOSPORT',
+            date: 'OCT 2015 - AGO 2016',
+            description: [
+                {
+                    id: 4,
+                    desc: `Diseño de campañas publicitarias para los relojes Technosport y marcas asociadas para Venezuela y otros países como Panamá, España y Mexico.`
                 },
                 {
-                    id: 7,
-                    desc:  `Working on the cloud side such as Amazon Web Services..`
+                    id: 4,
+                    desc: `Diseño y ajustes de materiales visuales para diferentes formatos, impresos, gigantografías, material web, redes sociales, diseño y diagramación de catálogos y retoque fotográfico.`
                 },
+               
             ]
-        }
+        },
     ])
 
 
